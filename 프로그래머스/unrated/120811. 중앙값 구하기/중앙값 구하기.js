768018d418606a3ee73f2ca69faae8a62900c7fe
Kal-MH <rkfalgus713@gmail.com>
2023-11-12 16:12:01 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,3 +1,7 @@
 function solution(array) {
-    return array.sort((a, b) => a - b)[Math.floor(array.length/2)]
+     if (!Array.isArray(array)) throw new Error("Error: Not an Array");
+  if (array.length % 2 === 0)
+    throw new Error("Error: Array Length must be odd");
+
+  return array.sort((a, b) => a - b)[Math.floor(array.length / 2)];
 }
